7ce93546415882a480a90b3a52a5890acd02db35
juaning <juaning@gmail.com>
2015-06-10 18:14:46 +0700

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Juan Mignaco on 10/06/2015.
+//  Copyright (c) 2015 Juan Mignaco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDoList
+//
+//  Created by Juan Mignaco on 10/06/2015.
+//  Copyright (c) 2015 Juan Mignaco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
